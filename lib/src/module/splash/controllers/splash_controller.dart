0f8b8e503bcaa9e03c323c08d5a3c678c23b955e
shibl <shiblimahmud512@gmail.com>
2026-02-25 10:34:06 +0600

    splash controller logic applied

--- lib/src/module/splash/controllers/splash_controller.dart
+++ lib/src/module/splash/controllers/splash_controller.dart
@@ -1,5 +1,14 @@
 import 'package:al_muttaqee/src/core/base/base_controller.dart';
+import 'package:al_muttaqee/src/core/routes/app_pages.dart';
+import 'package:get/get.dart';
 
 class SplashController extends BaseController{
 
+  void onReady() async {
+    super.onReady();
+    Future.delayed(const Duration(seconds: 3),(){
+      Get.offAllNamed(Routes.dashboard);
+    });
+    // await load();
+  }
 }
